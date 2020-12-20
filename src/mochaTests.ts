66f8ceba8ba134350f79552284a145e4f6b28214
DomHi <49837942+DomHi@users.noreply.github.com>
2020-12-20 12:36:42 +0100

    allow execution of all tests

--- src/mochaTests.ts
+++ src/mochaTests.ts
@@ -1,23 +1,17 @@
 import Mocha from 'mocha'
 import glob from 'glob'
 
-function usage(): void {
-    console.log('Usage: npm run test <puzzleID>')
-}
-
 function runTests() {
 
     let puzzle: string = process.argv[2]
 
-    if (!puzzle) {
-        console.log('Not enough arguments.')
-        usage()
-        return
-    }
-
     let mocha = new Mocha()
 
-    glob(__dirname + '/app/puzzles/' + puzzle + '/**/*.spec.js', (err, files) => {
+    let globPattern = __dirname + '/app/puzzles/'
+        + (puzzle ? puzzle + '/' : '')
+        + '**/*.spec.js'
+
+    glob(globPattern, (err, files) => {
         files.forEach(
             f => mocha.addFile(f)
         )
